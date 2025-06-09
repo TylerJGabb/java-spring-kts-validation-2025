5eb81e65eabafee8eac393df72a32513b8d02489
Tyler Gabb <tylerjgabb@gmail.com>
2025-06-08 17:26:32 -0700

    add explicit schema to prevent garbage value used as example value

--- src/main/kotlin/com/example/demo/dto/ExampleDto.kt
+++ src/main/kotlin/com/example/demo/dto/ExampleDto.kt
@@ -2,17 +2,23 @@ package com.example.demo.dto
 
 import com.example.demo.validation.OnCreate
 import com.example.demo.validation.OnUpdate
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Pattern
 
 data class ExampleDto(
     var id: Long? = null,
+
     @field:NotBlank(groups = [OnCreate::class], message = "Name is required for creation")
+    @field:Schema(
+        example = "Sample Name"
+    )
     @field:Pattern(
         regexp = "^[A-Za-z0-9 ]+$",
         groups = [OnCreate::class, OnUpdate::class],
         message = "Name can only contain letters, numbers, and spaces"
     )
     var name: String? = null,
+
     var description: String? = null
 )
